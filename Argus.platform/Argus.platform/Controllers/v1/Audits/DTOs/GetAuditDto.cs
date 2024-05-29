e0fb044be9658d8e21483186a3ece64dffe4dcd1
Chanaka <HP@LAPTOP-R5L5IOK2>
2024-05-29 14:08:45 +0530

    Add Git Ignore

---
+++ Argus.platform/Argus.platform/Controllers/v1/Audits/DTOs/GetAuditDto.cs
@@ -0,0 +1,31 @@
+﻿using Argus.Platform.Controllers.v1.Documents.DTOs;
+using Argus.Platform.Core.Complience.Audits;
+
+namespace Argus.Platform.Controllers.v1.Audits.DTOs
+{
+    public class GetAuditDto
+    {
+        public Guid BuyerId { get; set; }
+        public string Name { get; set; }
+        public int Frequency { get; set; }
+
+        public List<AuditRequirementsDetailDto> AuditRequirements { get; set; }
+    }
+
+    public class AuditRequirementsDetailDto
+    {
+        public Guid DocumentId { get; set; }
+
+        public string DocumentName { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime? ValidUntil { get; set; }
+        public string Status { get; set; }
+        public Guid AuditId { get; set; }
+    }
+
+   
+
+
+}
